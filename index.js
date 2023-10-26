46f1355a3eb7eec5bb952c57703bf367666e1908
Abdiqani <abdiqaniadan254@gmail.com>
2023-10-26 15:35:55 +0300

    commit

--- index.js
+++
@@ -1,9 +0,0 @@
-function populateMovieMenu() {
-
-
-    fetch('   http://localhost:3000/modelos') 
-        .then(response => response.json())
-        .then(data => console.log(data))
-            
-        
-    }
---
+++ shop.js
@@ -0,0 +1,1 @@
+ console.log (fetch ('http://localhost:3000/modelos'))
