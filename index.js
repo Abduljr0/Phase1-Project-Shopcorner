f7c2af6be9b03dbbbda0546ea9ab9aea149d66bb
Abdiqani <abdiqaniadan254@gmail.com>
2023-10-27 08:30:07 +0300

    final commit

--- index.js
+++ index.js
@@ -39,7 +39,42 @@ function populateModelList() {
     }
   
   });
-
+   
 
   window.onload = populateModelList;
+   
+  const searchInput = document.getElementById("searchInput");
+  const searchResults = document.getElementById("searchResults");
+  const cardsContainer = document.getElementById("cardsContainer");
+
+  // Function to filter items based on the search keyword
+  function updateSearchResults() {
+    const keyword = searchInput.value.toLowerCase();
+    searchResults.innerHTML = ""; // Clear the results
+
+    models.forEach((model) => {
+      if (model.nome.toLowerCase().includes(keyword)) {
+        const listItem = document.createElement("li");
+        listItem.textContent = model.nome;
+        listItem.addEventListener("click", () => {
+          // When a list item is clicked, display the details in the cardsContainer
+          cardsContainer.innerHTML =+`
+            <div class="card">
+              <img src="${model.img}" alt="${model.nome}">
+              <h5>${model.nome}</h5>
+              <p>Price: $${model.price}</p>
+            </div>
+          `;
+        });
+        searchResults.appendChild(listItem);
+      }
+    });
+  }
+
+  // Attach an event listener to the input field for real-time search
+  searchInput.addEventListener("input", updateSearchResults);
+
+  // Initial display of cards
+  cardsContainer.innerHTML = "Select a model from the list to display its details.";
+
 
